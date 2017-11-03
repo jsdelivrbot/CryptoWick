450936b78ca2be669d81f9147d682a046b9e7c09
Cole Dean Shepherd <cole@coledeanshepherd.com>
2017-11-03 00:53:09 -0500

    Started recording user sign-ups through email.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var nodemailer = require("nodemailer");
+var bodyParser = require("body-parser");
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -11,6 +14,37 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.post('/signup', function(request, response) {
+  let transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.EMAIL_ADDRESS,
+      clientId: process.env.EMAIL_OAUTH2_CLIENT_ID,
+      clientSecret: process.env.EMAIL_OAUTH2_CLIENT_SECRET,
+      refreshToken: process.env.EMAIL_OAUTH2_REFRESH_TOKEN
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: process.env.EMAIL_ADDRESS,
+      to: process.env.EMAIL_ADDRESS,
+      subject: 'CryptoWick Email Sign Up',
+      text: request.body.emailAddress + " has signed up for CryptoWick."
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);
+  });
+  
+  response.sendStatus(200);
 });
 
 app.listen(app.get('port'), function() {
